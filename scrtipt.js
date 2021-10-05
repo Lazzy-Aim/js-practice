ddff271236041b52fed3a2fa1416f43886de797c
Andrii Morozov <lazzy.aim@gmail.com>
2021-10-05 21:28:05 +0300

    Commit
    v1

---
+++ scrtipt.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/*
+let i = 10;
+
+if (1 < 2){
+    let i = 0;
+    console.log(i);
+}
+
+console.log(i);
+
+for(let i = 0; i<10; i++);
+console.log('Hello, world!' + i);
+*/
+
+/*
+let elem = document.querySelector('body');
+but.style.backgroundColor = 'coral';
+
+
+but.addEventListener('click',
+    function(){
+        elem.style.backgroundColor = 'red';
+    }
+);
+
+let elemP = document.querySelector('p');
+elemP.addEventListener('click', function()
+{
+    but.style.display = 'none'
+});
+*/
